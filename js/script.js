ac4efbe001d40e35238e5418a4c3b8f407eb7775
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-03 16:47:20 -0300

    feat: create two functions
    Those two functions are randomNumberBetween() and getComputerChoice()
    which this one try to call the randomNumberBetween().
    
    A variable in the main scope calls getComputerChoice() function.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
+/* FUNCTIONS */
+
+// CREATE a Function called randomNumberBetween() with max and min and returns a result.
+function randomNumberBetween(max, min)
+{
+    // This operation is kind of precision.
+    return Math.floor(Math.random() * (max - min + 1) + 1);
+}
+
+// CREATE a Function called getComputerChoice() with no parameters and returns a result.
+function getComputerChoice()
+{
+    // SET result to 0.
+    let result = 0;
+    // Round to the floor the operation.
+    result = randomNumberBetween(3, 1);
+    /*
+        Number 1: Rock.
+        Number 2: Paper.
+        Number 3: Scissor.
+    */
+    // Conditionals:
+    // A rock:
+    if (result === 1)
+    {
+        return 'ROCK';
+    }
+    // A paper:
+    else if (result === 2)
+    {
+        return 'PAPER';
+    }
+    // A scissor:
+    else
+    {
+        return 'SCISSOR';
+    }
+}
+
+// -------------
+
 // Where the logic begins:
 console.log('ROCK, PAPERS, SCISSORS!');
 
@@ -6,5 +47,7 @@ console.log('ROCK, PAPERS, SCISSORS!');
 let playerSelection = '';
 // SET computerSelection to empty text.
 let computerSelection = '';
+// ASSIGN computerSelection called a function getComputerChoice().
+computerSelection = getComputerChoice();
 // PRINT a Function called playRound() with playerSelection and computerSelection Returning "result":
 console.log('');
