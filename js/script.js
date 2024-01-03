c18b2db1ffdcfc7a2204aca7c7ae0399b878924e
Fernando Cardozo <nuevo_10000@outlook.com>
2024-01-03 16:36:59 -0300

    feat: add two variables and an output

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // Where the logic begins:
 console.log('ROCK, PAPERS, SCISSORS!');
+
+// Main variables:
+// SET playerSelection to empty text.
+let playerSelection = '';
+// SET computerSelection to empty text.
+let computerSelection = '';
+// PRINT a Function called playRound() with playerSelection and computerSelection Returning "result":
+console.log('');
